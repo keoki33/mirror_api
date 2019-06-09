05ad89756d0bfbf334a067fd9d36b46b584c7f25
keoki33 <gwong323@yahoo.com>
2019-06-09 15:25:02 +0100

    action cable config foir heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = "wss://boiling-tundra-71042.herokuapp.com/cable" 
+config.action_cable.allowed_request_origins = [%r{https?://\S+}]
 end
